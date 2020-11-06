580d4a92ba8ad3e37dd894f7ff758f124c265b0e
alxnns1 <alxnns1@live.co.uk>
2020-11-06 23:51:00 +0000

    Reset of the tools

--- src/main/java/com/alxnns1/mobhunter/init/MHItems.java
+++ src/main/java/com/alxnns1/mobhunter/init/MHItems.java
@@ -10,59 +10,86 @@ public class MHItems {
 
 	public static Item[] register() {
 		//TOOLS
-		registerTool("machalite_pickaxe", ItemTier.IRON, 1, -2.8F, MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerTool("dragonite_pickaxe", ItemTier.IRON, 1, -2.8F, MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerTool("carbalite_pickaxe", ItemTier.DIAMOND, 1, -2.8F, MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerTool("eltalite_pickaxe", ItemTier.NETHERITE, 1, -2.8F, MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
+		registerPickaxe("machalite_pickaxe", ItemTier.IRON);
+		registerPickaxe("dragonite_pickaxe", ItemTier.IRON);
+		registerPickaxe("carbalite_pickaxe", ItemTier.DIAMOND);
+		registerPickaxe("eltalite_pickaxe", ItemTier.NETHERITE);
+		registerAxe("machalite_axe", ItemTier.IRON);
+		registerAxe("dragonite_axe", ItemTier.IRON);
+		registerAxe("carbalite_axe", ItemTier.DIAMOND);
+		registerAxe("eltalite_axe", ItemTier.NETHERITE);
+		registerShovel("machalite_shovel", ItemTier.IRON);
+		registerShovel("dragonite_shovel", ItemTier.IRON);
+		registerShovel("carbalite_shovel", ItemTier.DIAMOND);
+		registerShovel("eltalite_shovel", ItemTier.NETHERITE);
+		registerHoe("machalite_hoe", ItemTier.IRON);
+		registerHoe("dragonite_hoe", ItemTier.IRON);
+		registerHoe("carbalite_hoe", ItemTier.DIAMOND);
+		registerHoe("eltalite_hoe", ItemTier.NETHERITE);
 		//ORES
-		registerItem("earth_crystal", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("machalite_ingot", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("ice_crystal", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("bealite_ingot", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("lightcrystal", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("dragonite_ingot", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("firestone", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("carbalite_ingot", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("eltalite_ingot", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("meldspar_ingot", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("novacrystal", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("purecrystal", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("fucium_ingot", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("firecell_stone", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("allfire_stone", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("ultimas_crystal", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
+		registerItem("earth_crystal");
+		registerItem("machalite_ingot");
+		registerItem("ice_crystal");
+		registerItem("bealite_ingot");
+		registerItem("lightcrystal");
+		registerItem("dragonite_ingot");
+		registerItem("firestone");
+		registerItem("carbalite_ingot");
+		registerItem("eltalite_ingot");
+		registerItem("meldspar_ingot");
+		registerItem("novacrystal");
+		registerItem("purecrystal");
+		registerItem("fucium_ingot");
+		registerItem("firecell_stone");
+		registerItem("allfire_stone");
+		registerItem("ultimas_crystal");
 		//FISH
-		registerItem("whetfish", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("sushifish", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("sleepyfish", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("pin_tuna", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("speartuna", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("popfish", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("scatterfish", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("burst_arowana", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("bomb_arowana", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("glutton_tuna", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("gastronome_tuna", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("ancient_fish", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("small_goldenfish", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("wanchovy", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("guardfish", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("brocadefish", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("goldenfish", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("king_brocadefish", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("premium_sashimi", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
-		registerItem("silverfish", MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP);
+		registerItem("whetfish");
+		registerItem("sushifish");
+		registerItem("sleepyfish");
+		registerItem("pin_tuna");
+		registerItem("speartuna");
+		registerItem("popfish");
+		registerItem("scatterfish");
+		registerItem("burst_arowana");
+		registerItem("bomb_arowana");
+		registerItem("glutton_tuna");
+		registerItem("gastronome_tuna");
+		registerItem("ancient_fish");
+		registerItem("small_goldenfish");
+		registerItem("wanchovy");
+		registerItem("guardfish");
+		registerItem("brocadefish");
+		registerItem("goldenfish");
+		registerItem("king_brocadefish");
+		registerItem("premium_sashimi");
+		registerItem("silverfish");
 
 		return ITEMS.values().toArray(new Item[0]);
 	}
 
-	private static void registerItem(String name, ItemGroup itemGroup) {
-		Item item = new Item(new Item.Properties().group(itemGroup)).setRegistryName(name);
+	private static void registerItem(String name) {
+		Item item = new Item(new Item.Properties().group(MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP)).setRegistryName(name);
 		ITEMS.put(name, item);
 	}
 
-	private static void registerTool(String name, IItemTier itemTier, int attackDamage, float attackSpeed, ItemGroup itemGroup) {
-		Item item = new PickaxeItem(itemTier, attackDamage, attackSpeed, new Item.Properties().group(itemGroup)).setRegistryName(name);
+	private static void registerPickaxe(String name, IItemTier itemTier) {
+		Item item = new PickaxeItem(itemTier, 1, -2.8F, new Item.Properties().group(MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP)).setRegistryName(name);
+		ITEMS.put(name, item);
+	}
+
+	private static void registerAxe(String name, IItemTier itemTier) {
+		Item item = new AxeItem(itemTier, 6F, -3.1F, new Item.Properties().group(MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP)).setRegistryName(name);
+		ITEMS.put(name, item);
+	}
+
+	private static void registerShovel(String name, IItemTier itemTier) {
+		Item item = new ShovelItem(itemTier, 1.5F, -3F, new Item.Properties().group(MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP)).setRegistryName(name);
+		ITEMS.put(name, item);
+	}
+
+	private static void registerHoe(String name, IItemTier itemTier) {
+		Item item = new HoeItem(itemTier, 3, 0F, new Item.Properties().group(MHItemGroups.MOB_HUNTER_ITEMS_ITEM_GROUP)).setRegistryName(name);
 		ITEMS.put(name, item);
 	}
 }
